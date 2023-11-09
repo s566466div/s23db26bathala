8eb047f4c9063f6b19fdf33d92c569cb42ce5512
Divya Bathala <S566466@nwmissouri.edu>
2023-11-08 23:28:07 -0600

    created new files and updatedin app

--- controllers/fruits.js
+++ controllers/fruits.js
@@ -8,9 +8,27 @@ exports.fruits_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: fruits detail: ' + req.params.id);
 };
 // Handle fruits create on POST.
-exports.fruits_create_post = function(_req, res) {
- res.send('NOT IMPLEMENTED: fruits create POST');
+
+exports.fruits_create_post = async function(req, res) {
+        console.log(req.body)
+        let document = new fruits();
+        // We are looking for a body, since POST does not have query parameters.
+        // Even though bodies can be in many different formats, we will be picky
+        // and require that it be a json object
+        // {"costume_type":"goat", "cost":12, "size":"large"}
+        document.name = req.body.name;
+        document.color = req.body.color;
+        document.cost = req.body.cost;
+        try{
+        let result = await document.save();
+        res.send(result);
+        }
+        catch(err){
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+        }
 };
+ 
 // Handle fruits delete form on DELETE.
 exports.fruits_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: fruits delete DELETE ' + req.params.id);
@@ -45,22 +63,3 @@ exports.fruits_list = async function (req, res) {
 };
 
 // Handlefruits create on POST.
-exports.fruits_create_post = async function(req, res) {
-    console.log(req.body)
-    let document = new fruits();
-    // We are looking for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
-    document.name = req.body.name;
-    document.color = req.body.color;
-    document.cost = req.body.cost;
-    try{
-    let result = await document.save();
-    res.send(result);
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-   };
