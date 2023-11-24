7604db97200cbde459fe810efa783e11d06138fe
Divya Bathala <S566466@nwmissouri.edu>
2023-11-24 15:28:40 -0600

    -m "updated fruitsjs"

--- routes/fruits.js
+++ routes/fruits.js
@@ -15,13 +15,13 @@ const secured = (req, res, next) => {
 /* GET home page. */
 router.get('/', fruits_controllers.fruits_view_all_Page );
 /* GET detail fruits page */
-router.get('/detail', fruits_controllers.fruits_view_one_Page);
+router.get('/detail',secured,fruits_controllers.fruits_view_one_Page);
 /* GET create fruits page */
-router.get('/create', fruits_controllers.fruits_create_Page);
+router.get('/create',secured, fruits_controllers.fruits_create_Page);
 /* GET create update page */
 router.get('/update',secured, fruits_controllers.fruits_update_Page);
 /* GET delete Fruits page */
-router.get('/delete', fruits_controllers.fruits_delete_Page);
+router.get('/delete',secured, fruits_controllers.fruits_delete_Page);
 router.post('/login', passport.authenticate('local'), function(req, res) {
     res.redirect('/');
    });
